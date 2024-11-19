b9dd439404a34d33ac2ba1ecbc23458cefaca2b0
viniciusfonseca <vfonseca1618@gmail.com>
2024-11-18 22:30:25 -0300

    add get amigo secreto

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const db = new Database(":memory:");
 
 db.exec(`CREATE TABLE IF NOT EXISTS GRUPOS (
   ID TEXT,
+  SORTEIO TEXT,
   PRIMARY KEY(ID)
 );`)
 
@@ -15,7 +16,9 @@ db.exec(`CREATE TABLE IF NOT EXISTS PARTICIPANTES (
   PRIMARY KEY(NOME, ID_GRUPO)
 );`)
 
-class Participante { nome?: String }
+class Grupo { sorteio?: string }
+class Participante { nome?: string }
+class Sorteio { participante?: string }
 
 new Elysia()
 	.post('/grupos', () => {
@@ -25,13 +28,21 @@ new Elysia()
     return { id }
   })
 	.post('/grupos/:id_grupo/participantes', ({ body: { nome }, params }) =>{
-    db.prepare(`INSERT INTO PARTICIPANTES (NOME, ID_GRUPO) VALUES ($nome, $id_grupo);`)
+    db.prepare(
+      `INSERT INTO PARTICIPANTES (NOME, ID_GRUPO)
+      VALUES ($nome, $id_grupo);`
+    )
       .all({ $nome: nome, $id_grupo: params.id_grupo })
   }, {
     body: t.Object({ nome: t.String() })
   })
-	.post('/grupos/:id_grupo/sorteios', ({ params }) => {
-    return db.prepare(`SELECT NOME as nome, RANDOM() FROM PARTICIPANTES WHERE ID_GRUPO = $id_grupo ORDER BY 2;`)
+	.post('/grupos/:id_grupo/sorteios', ({ params, set }) => {
+    const sorteio = db.prepare(`
+      SELECT NOME as nome, RANDOM()
+      FROM PARTICIPANTES
+      WHERE ID_GRUPO = $id_grupo
+      ORDER BY 2;`
+    )
       .as(Participante)
       .all({ $id_grupo: params.id_grupo })
       .map((participante, i, array) => {
@@ -41,5 +52,28 @@ new Elysia()
           amigo_secreto: array[j].nome
         }
       })
+    db.prepare(`UPDATE GRUPOS SET SORTEIO = $sorteio WHERE ID = $id_grupo;`)
+      .all({
+        $id_grupo: params.id_grupo,
+        $sorteio: JSON.stringify(sorteio)
+      })
+    set.status = 201
+  })
+  .get('/grupos/:id_grupo/participantes/:nome/amigo_secreto', ({ params, set }) => {
+    const grupos = db.prepare(`SELECT SORTEIO as sorteio FROM GRUPOS WHERE ID = $id_grupo;`)
+      .as(Grupo)
+      .all({ $id_grupo: params.id_grupo })
+    if (grupos.length === 0) {
+      set.status = 404
+      return
+    }
+    try {
+      const parsedSorteio = JSON.parse(grupos[0].sorteio!) as Sorteio[]
+      const participante = parsedSorteio.find(sorteio => sorteio.participante === params.nome)
+      if (participante === undefined) { set.status = 404; return }
+      return participante
+    }
+    catch (e) {}
+    set.status = 404; return
   })
 	.listen(3000)
